31ee6d2c37f502aed6230163672d05dc4aa287f6
Tossie85 <Toshi_IMAI@outlook.jp>
2020-06-07 12:16:10 +0900

    first create

---
+++ FormMain.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace MeetingTimer
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.lbTime = new System.Windows.Forms.Label();
+            this.textBoxNumber = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxPrice = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.lbPrice = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lbTime
+            // 
+            this.lbTime.AutoSize = true;
+            this.lbTime.Font = new System.Drawing.Font("Cascadia Code", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTime.Location = new System.Drawing.Point(12, 9);
+            this.lbTime.Name = "lbTime";
+            this.lbTime.Size = new System.Drawing.Size(498, 116);
+            this.lbTime.TabIndex = 0;
+            this.lbTime.Text = "00:00:00";
+            // 
+            // textBoxNumber
+            // 
+            this.textBoxNumber.Font = new System.Drawing.Font("MS UI Gothic", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBoxNumber.Location = new System.Drawing.Point(242, 129);
+            this.textBoxNumber.Name = "textBoxNumber";
+            this.textBoxNumber.Size = new System.Drawing.Size(100, 55);
+            this.textBoxNumber.TabIndex = 1;
+            this.textBoxNumber.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(24, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(212, 48);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "参加人数";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(348, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 48);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "人";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS UI Gothic", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label4.Location = new System.Drawing.Point(24, 198);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(116, 48);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "単価";
+            // 
+            // textBoxPrice
+            // 
+            this.textBoxPrice.Font = new System.Drawing.Font("MS UI Gothic", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBoxPrice.Location = new System.Drawing.Point(142, 195);
+            this.textBoxPrice.Name = "textBoxPrice";
+            this.textBoxPrice.Size = new System.Drawing.Size(200, 55);
+            this.textBoxPrice.TabIndex = 2;
+            this.textBoxPrice.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("MS UI Gothic", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label5.Location = new System.Drawing.Point(348, 198);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(140, 48);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "円/時";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.buttonStart.Location = new System.Drawing.Point(338, 276);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(150, 50);
+            this.buttonStart.TabIndex = 3;
+            this.buttonStart.Text = "START";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.ButtonStart_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.buttonStop.Location = new System.Drawing.Point(175, 276);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(150, 50);
+            this.buttonStop.TabIndex = 4;
+            this.buttonStop.Text = "STOP";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.ButtonStop_Click);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.buttonReset.Location = new System.Drawing.Point(12, 276);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(150, 50);
+            this.buttonReset.TabIndex = 5;
+            this.buttonReset.Text = "RESET";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.ButtonReset_Click);
+            // 
+            // lbPrice
+            // 
+            this.lbPrice.AutoSize = true;
+            this.lbPrice.Font = new System.Drawing.Font("Cascadia Code", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPrice.Location = new System.Drawing.Point(17, 328);
+            this.lbPrice.MinimumSize = new System.Drawing.Size(400, 0);
+            this.lbPrice.Name = "lbPrice";
+            this.lbPrice.Size = new System.Drawing.Size(400, 116);
+            this.lbPrice.TabIndex = 0;
+            this.lbPrice.Text = "0";
+            this.lbPrice.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("MS UI Gothic", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label6.Location = new System.Drawing.Point(420, 380);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(68, 48);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "円";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxPrice);
+            this.Controls.Add(this.textBoxNumber);
+            this.Controls.Add(this.lbPrice);
+            this.Controls.Add(this.lbTime);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormMain";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbTime;
+        private System.Windows.Forms.TextBox textBoxNumber;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxPrice;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.Label lbPrice;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ FormMain.cs
@@ -0,0 +1,148 @@
+﻿/// 
+/// 会議タイマー
+/// Auther: Tossie85
+/// Created: 2020.6.7
+///
+using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace MeetingTimer
+{
+    public partial class FormMain : Form
+    {
+        private Stopwatch watch;
+        int unitPrice = 0;
+        int number = 0;
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// フォームの初期化
+        /// </summary>
+        private void InitForm()
+        {
+            Text = Properties.Resources.AppName;
+            lbTime.Text = "00:00:00";
+            lbPrice.Text = "0";
+
+            textBoxNumber.Enabled = true;
+            textBoxPrice.Enabled = true;
+            buttonReset.Enabled = true;
+            buttonStop.Enabled = false;
+            buttonStart.Enabled = true;
+        }
+
+        /// <summary>
+        /// フォームロードイベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            InitForm();
+        }
+
+
+        /// <summary>
+        /// スタートボタンクリックイベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButtonStart_Click(object sender, EventArgs e)
+        {
+            if (textBoxNumber.Text.Trim().Equals(""))
+            {
+                MessageBox.Show("参加人数が入力されていません。");
+                textBoxNumber.Focus();
+                return;
+            }
+            if (textBoxPrice.Text.Trim().Equals(""))
+            {
+                MessageBox.Show("単価が入力されていません。");
+                textBoxPrice.Focus();
+                return;
+            }
+            if(!int.TryParse(textBoxNumber.Text.Trim(), out number))
+            {
+                MessageBox.Show("参加人数が整数値ではありません。");
+                textBoxNumber.Focus();
+                return;
+            }
+            if (!int.TryParse(textBoxPrice.Text.Trim(), out unitPrice))
+            {
+                MessageBox.Show("単価が整数値ではありません。");
+                textBoxPrice.Focus();
+                return;
+            }
+            textBoxNumber.Enabled = false;
+            textBoxPrice.Enabled = false;
+            buttonReset.Enabled = false;
+            buttonStop.Enabled = true;
+            buttonStart.Enabled = false;
+            timer1.Start();
+            watch = new Stopwatch();
+            watch.Start();
+        }
+
+        /// <summary>
+        /// ストップボタンクリックイベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButtonStop_Click(object sender, EventArgs e)
+        {
+            if(watch != null)
+            {
+                watch.Stop();
+                timer1.Stop();
+
+                textBoxNumber.Enabled = false;
+                textBoxPrice.Enabled = false;
+                buttonReset.Enabled = true;
+                buttonStop.Enabled = false;
+                buttonStart.Enabled = true;
+            }
+        }
+
+        /// <summary>
+        /// タイマーイベント
+        /// タイマーの表示更新、積算金額の更新
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            TimeSpan tim = watch.Elapsed;
+            lbTime.Text = string.Format("{0:00}:{1:00}:{2:00}", tim.Hours, tim.Minutes, tim.Seconds);
+            int sec = tim.Seconds;
+            int min = tim.Minutes;
+            int hour = tim.Hours;
+            double secPrice = unitPrice / 3600;
+            double minPrice = unitPrice / 60;
+            double price = (this.unitPrice * hour + minPrice * min + secPrice * sec) * this.number;
+            lbPrice.Text = Math.Round(price).ToString();
+        }
+
+        /// <summary>
+        /// リセットボタンクリックイベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButtonReset_Click(object sender, EventArgs e)
+        {
+            watch.Reset();
+            lbPrice.Text = "0";
+            lbTime.Text = "00:00:00";
+            watch = null;
+            textBoxNumber.Enabled = true;
+            textBoxPrice.Enabled = true;
+            buttonReset.Enabled = true;
+            buttonStop.Enabled = false;
+            buttonStart.Enabled = true;
+        }
+    }
+}
